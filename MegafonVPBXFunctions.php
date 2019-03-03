a65b4e22e24e34ff1e4c46e5c78a145efc1834ee
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2019-03-03 14:42:54 +0300

    Создание репозитория MegafonVPBX

---
+++ MegafonVPBXFunctions.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Интеграция CRM Clientbase с Виртуальной АТС Мегафон
+  // https://ClientbasePro.ru
+  // https://www.megapbx.ru/rest_api
+  
+require_once 'common.php'; 
+
+
+
+
+
+
+
+
+?>
